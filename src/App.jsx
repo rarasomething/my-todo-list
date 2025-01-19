de11940fb790b3847ffd0320ea7047789fd8820a
rarasomething <79035051+rarasomething@users.noreply.github.com>
2025-01-19 22:03:46 +0900

    25-01-19

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return <></>;
+  const [todoList, setTodoList] = useState([]);
+  const [value, setValue] = useState("");
+
+  const onButtonClick = () => {
+    setTodoList([...todoList, value]);
+  };
+
+  const onInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((e) => (
+          <li>{e}</li>
+        ))}
+      </ul>
+
+      <input onChange={onInputChange} />
+      <button onClick={onButtonClick}>Add</button>
+    </>
+  );
 }
 
-export default App
+export default App;
